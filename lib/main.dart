d2b086ed98446f70e5320628a41dff0c6615fde3
Krapowkai <thyrt104@gmail.com>
2023-10-31 02:24:03 +0700

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,7 @@ import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: const FirebaseOptions(
-      apiKey: 'AIzaSyDL5neQrF-c-xTYjqkjFA2wmkg0AeS9SvU', 
-      appId: '1:1049290280710:android:7030d78a37161abf9ad887', 
-      messagingSenderId: '1049290280710', 
-      projectId: 'doublem-91fd8'
-    ),
-  );
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 class MyApp extends StatelessWidget{
